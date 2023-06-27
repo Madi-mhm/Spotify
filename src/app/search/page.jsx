b80b90dea732f747970d84b5ac06c19c883d8066
Mahdi <mahdi.unic1@gmail.com>
2023-06-27 12:18:41 +0200

    commit before starting searchBar API

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,19 +1,32 @@
+"use client"
+
 import './page.scss'
 import { BiSearch } from "react-icons/bi";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import SmallCards from '@/components/Cards/SmallCards';
 import ArtistProfile from '@/components/ArtistProfile/ArtistProfile';
+import { useState } from 'react';
 
 
 const SearchPage = () =>{
+
+    const [search, setSearch] = useState()
+
+    const handleSearchInput = (event)=>{
+        setSearch(event.target.value)
+    }
+
+    const handleSearchButton = () =>{
+        console.log(search)
+    }
 
     return (
         <main className='searchMainPage'>
             <div className="searchPage">
                 <div className="searchSection">
-                    <p><AiOutlineArrowLeft/></p>
-                    <input type="text" placeholder="search your artist..." />
-                    <span><BiSearch/></span>
+                    <a href='./'><AiOutlineArrowLeft/></a>
+                    <input type="text" placeholder="search your artist..." onChange={handleSearchInput}/>
+                    <span onClick={handleSearchButton}><BiSearch/></span>
 
                 </div>
             </div>
@@ -22,3 +35,16 @@ const SearchPage = () =>{
 }
 
 export default SearchPage
+
+
+
+//     const handleSearch = () =>{
+//         const filteredData = data.people.filter((item)=>{
+//             return item.name.includes(search)
+//         })
+
+//         console.log(filteredData)
+//     }
+
+
+
