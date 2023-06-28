af1a43f7ba65c772df330b34aba51fbf6f88d4aa
Mahdi <mahdi.unic1@gmail.com>
2023-06-28 18:35:18 +0200

    changed the size of images for carousels

--- src/components/Carousels/NewReleases.jsx
+++ src/components/Carousels/NewReleases.jsx
@@ -6,16 +6,28 @@ import './newReleases.scss'
 const NewReleasesSection = async () => {
   const apiData = await fetchSpotifyApi('browse/new-releases?country=US&offset=0');
 
+
+  // const imageURL = apiData.albums.items.map((item)=>{
+  //   return item.images[0]
+  // })
+  // console.log(imageURL);
+ 
+
+
   return (
     <div className='carouselCountainer'>
       {apiData?.albums.items.map((item) => {
-        let imageUrl = '';
+        let imageUrl = "";
+
+        // find images with 300px Width 
+        const image300 = item.images.find((image)=> image.width === 300)
+        if(image300){
+          imageUrl = image300.url
+        }
+
+
         return (
           <React.Fragment key={item.id}>
-            {item.images.map((i) => {
-              imageUrl = i.url;
-              return null;
-            })}
             <BigCards name={item.name} imageUrl={imageUrl} type={item.type} albumId={item.id} />
           </React.Fragment>
         );
